729c367f79a905e71a511eeef549292f1fc46917
buanv <vanessabuan29@gmail.com>
2026-03-11 13:48:20 +0800

    Add files via upload

---
+++ final_project/final_project/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace final_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblUsername = new Label();
+            lblPass = new Label();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            btnLogin = new Button();
+            SuspendLayout();
+            // 
+            // lblUsername
+            // 
+            lblUsername.AutoSize = true;
+            lblUsername.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            lblUsername.Location = new Point(10, 13);
+            lblUsername.Name = "lblUsername";
+            lblUsername.Size = new Size(67, 15);
+            lblUsername.TabIndex = 0;
+            lblUsername.Text = "Username:";
+            // 
+            // lblPass
+            // 
+            lblPass.AutoSize = true;
+            lblPass.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            lblPass.Location = new Point(12, 48);
+            lblPass.Name = "lblPass";
+            lblPass.Size = new Size(62, 15);
+            lblPass.TabIndex = 1;
+            lblPass.Text = "Password:";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(76, 9);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(168, 23);
+            txtUsername.TabIndex = 2;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(76, 45);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PasswordChar = '●';
+            txtPassword.Size = new Size(168, 23);
+            txtPassword.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(10, 79);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(238, 38);
+            btnLogin.TabIndex = 4;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(261, 147);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Controls.Add(lblPass);
+            Controls.Add(lblUsername);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblUsername;
+        private Label lblPass;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private Button btnLogin;
+    }
+}
